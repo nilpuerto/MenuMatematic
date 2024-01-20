faa096dbeb5ae8b78c2c6b6179bd6eae3fa63a08
nil <nilpuerrtomarques@gmail.com>
2024-01-20 13:01:22 +0100

    Projecte 90%fet, (falta el case 8)

--- Program.cs
+++ Program.cs
@@ -1,22 +1,12 @@
-﻿
-class Program
+﻿class Program
 {
-    //1.Calcular máxim
-    static int Maxim(int num1, int num2)
+    // 1.Calcular màxim
+    static int Maxim(int a, int b)
     {
-        if (num1 > num2)
-        {
-            return num1;
-        }
-        else
-        {
-            return num2;
-        }
+        return (a > b) ? a : b;
     }
 
-
-
-    // 2.Métode per calcular el MCD
+    // 2.Mètode per calcular el MCD
     static int CalcularMCD(int a, int b)
     {
         while (b != 0)
@@ -28,14 +18,14 @@ class Program
         return a;
     }
 
-    // 3.Métode per calcular el MCM
+    // 3.Mètode per calcular el MCM
     static int CalcularMCM(int a, int b)
     {
         int mcd = CalcularMCD(a, b);
         int mcm = (a * b) / ((mcd == 0) ? 1 : mcd);
-
         return mcm;
     }
+
     // 4.Calcular Factorial
     static double CalcularFactorial(double n)
     {
@@ -49,60 +39,100 @@ class Program
         }
     }
 
-    //Main
+    // 5. Calcular Combinatori
+    static double CalcularCombinatori(double n, int k)
+    {
+        double combinatori = CalcularFactorial(n) / (CalcularFactorial(k) * CalcularFactorial(n - k));
+        return combinatori;
+    }
+
+    // 6. Calcular Divisor Major
+    static int CalcularDivisorMajor(int a, int b)
+    {
+        int divisorMajor = 1;
+
+        for (int i = 1; i <= a && i <= b; i++)
+        {
+            if (a % i == 0 && b % i == 0)
+            {
+                divisorMajor = i;
+            }
+        }
+
+        return divisorMajor;
+    }
+
+    // 7. Calcular Numero Primer
+    static bool EsNumeroPrimer(int num)
+    {
+        if (num < 2)
+        {
+            return false;
+        }
+
+        //MATH SQRT per calcualr arrel quadrada
+        for (int i = 2; i <= Math.Sqrt(num); i++)
+        {
+            if (num % i == 0)
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+    // Main
     static void Main()
     {
-        //Variables
+        // Variables
         int opcio;
         int num1 = 0;
         int num2 = 0;
         double num = 0;
 
-        //Interfaz
+        // Interfaz
         do
         {
-            //Colors
+            // Colors
             Console.BackgroundColor = ConsoleColor.DarkRed;
             Console.Clear();
             Console.ForegroundColor = ConsoleColor.Yellow;
 
-
             // Quadre
             string[] MenuInterfaz = {
-            " ╔═════════════════════════════════════════════════════════╗ ",
-            " ║                    MENU MATEMATIC                       ║ ",
-            " ║=========================================================║ ",
-            " ║                    1) Maxim                             ║ ",
-            " ║                    2) MCD                               ║ ",
-            " ║                    3) MCM                               ║ ",
-            " ║                    4) Factorial                         ║ ",
-            " ║                    5) Combinatori                       ║ ",
-            " ║                    6) Divisor Major                     ║ ",
-            " ║                    7) Numero Primer                     ║ ",
-            " ║                    8) NPrimers                          ║ ",
-            " ║                    9) Sortir                            ║ ",
-            " ║=========================================================║ ",
-            " ║   - Escull una opcio -                                  ║ ",
-            " ║=========================================================║ ",
-            " ║                                             Nil i Alex  ║ ",
-            " ║_________________________________________________________║ "
-
+                    " ╔═════════════════════════════════════════════════════════╗ ",
+                    " ║                    MENU MATEMATIC                       ║ ",
+                    " ║=========================================================║ ",
+                    " ║                    1) Maxim                             ║ ",
+                    " ║                    2) MCD                               ║ ",
+                    " ║                    3) MCM                               ║ ",
+                    " ║                    4) Factorial                         ║ ",
+                    " ║                    5) Combinatori                       ║ ",
+                    " ║                    6) Divisor Major                     ║ ",
+                    " ║                    7) Numero Primer                     ║ ",
+                    " ║                    8) NPrimers                          ║ ",
+                    " ║                    9) Sortir                            ║ ",
+                    " ║=========================================================║ ",
+                    " ║   - Escull una opcio -                                  ║ ",
+                    " ║=========================================================║ ",
+                    " ║                                             Nil i Alex  ║ ",
+                    " ║_________________________________________________________║ "
             };
 
             // POSICIO EN VERTICAL
-            int centerY = (Console.WindowHeight - MenuInterfaz.Length) / 2;
+            int centreY = (Console.WindowHeight - MenuInterfaz.Length) / 2;
 
-            // Bucle per posar cada linea del menu centrada
+            // Bucle per posar cada línia del menú centrada
             foreach (string linea in MenuInterfaz)
             {
                 // Calcular la posició
-                int centerX = (Console.WindowWidth - linea.Length) / 2;
+                int centreX = (Console.WindowWidth - linea.Length) / 2;
 
-                Console.SetCursorPosition(centerX, centerY);
+                Console.SetCursorPosition(centreX, centreY);
                 Console.WriteLine(linea);
 
-                // Increment pe rla sseuent linea
-                centerY++;
+                // Increment per la següent línia
+                centreY++;
             }
 
             opcio = Convert.ToInt32(Console.ReadLine());
@@ -110,17 +140,17 @@ class Program
             // Seleccionar opció y preguntar números
             if (opcio > 0 && opcio < 9)
             {
-                // Preguntar per un sol número si tria el 3
-                if (opcio != 4)
+                // Preguntar per un sol número si tria el 4 o el 5
+                if (opcio != 4 && opcio != 5)
                 {
-                    Console.Write("Escriu un numero: ");
+                    Console.Write("Escriu un número: ");
                     num1 = Convert.ToInt32(Console.ReadLine());
-                    Console.Write("Escriu un altre numero: ");
+                    Console.Write("Escriu un altre número: ");
                     num2 = Convert.ToInt32(Console.ReadLine());
                 }
                 else
                 {
-                    Console.Write("Escriu un numero: ");
+                    Console.Write("Escriu un número: ");
                     num = Convert.ToDouble(Console.ReadLine());
                 }
             }
@@ -130,22 +160,42 @@ class Program
             {
                 case 1:
                     int maxim = Maxim(num1, num2);
-                    Console.WriteLine($"El Máxim de {num1} i {num2} es: {maxim}");
+                    Console.WriteLine($"El Màxim de {num1} i {num2} és: {maxim}");
                     break;
 
                 case 2:
                     int mcd = CalcularMCD(num1, num2);
-                    Console.WriteLine($"El MCD de {num1} i {num2} es: {mcd}");
+                    Console.WriteLine($"El MCD de {num1} i {num2} és: {mcd}");
                     break;
 
                 case 3:
                     int mcm = CalcularMCM(num1, num2);
-                    Console.WriteLine($"El MCM de {num1} i {num2} es: {mcm}");
+                    Console.WriteLine($"El MCM de {num1} i {num2} és: {mcm}");
                     break;
 
                 case 4:
                     double factorial = CalcularFactorial(num);
-                    Console.WriteLine($"El resultat Factorial de {num} es: {factorial}");
+                    Console.WriteLine($"El resultat Factorial de {num} és: {factorial}");
+                    break;
+
+                case 5:
+                    Console.Write("Escriu un altre valor: ");
+                    int valorP = Convert.ToInt32(Console.ReadLine());
+                    double combinatoriResultat = CalcularCombinatori(num, valorP);
+                    Console.WriteLine($"El Combinatori de C({num1}, {valorP}) és: {combinatoriResultat}");
+                    break;
+
+                case 6:
+                    int divisorMajor = CalcularDivisorMajor(num1, num2);
+                    Console.WriteLine($"El Divisor Major de {num1} i {num2} és: {divisorMajor}");
+                    break;
+
+                case 7:
+                    bool esPrimer1 = EsNumeroPrimer(num1);
+                    bool esPrimer2 = EsNumeroPrimer(num2);
+
+                    Console.WriteLine($"El número {num1} {(esPrimer1 ? "es" : "no es")} primer.");
+                    Console.WriteLine($"El número {num2} {(esPrimer2 ? "es" : "no es")} primer.");
                     break;
 
                 // Opció de sortida
@@ -154,19 +204,17 @@ class Program
                     break;
             }
 
-            // Aqui fem que em d'esperar 5 segons
+            // Aquí fem que esperi 5 segons
             if (opcio != 9)
             {
                 for (int i = 5; i > 0; i--)
                 {
                     Console.WriteLine($"Esperant {i} segons...");
                     Thread.Sleep(1000); // 1000 milisegons = 1 segon
-                   
                 }
 
                 Console.WriteLine("Esperant 0 segons...");
             }
         } while (opcio != 9);
-
     }
 }
